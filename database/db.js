20eba860ddb3ff8e49ee9359701453562f762426
DindawFly <97446180+DindawFlay@users.noreply.github.com>
2022-04-11 12:06:09 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://restapi.delnichi.xyz';
+var url = 'https://restapi-nazzsoft.herokuapp.com';
 try {
-if(url == 'https://restapi.delnichi.xyz') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://restapi-nazzsoft.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
